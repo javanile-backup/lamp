03f6625bae312220a42d86113f1e6ecb78e7ef79
francescobianco <info.francescobianco@gmail.com>
2017-08-22 00:12:34 +0200

    sync

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # docker-compose-lamp
 
 ```bash
+
+curl -sL //rawgit.com/javanile/docker-compose-lamp/master/setup.sh | bash -
+
+
 git clone https://github.com/javanile/docker-compose-lamp lamp
 ln -s lamp/docker-compose.yml docker-compose.yml
 ```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/javanile/docker-compose-lamp lamp
-ln -s lamp/docker-compose.yml docker-compose.yml
+ls
+
+#git clone https://github.com/javanile/docker-compose-lamp lamp
+#ln -s lamp/docker-compose.yml docker-compose.yml
 
