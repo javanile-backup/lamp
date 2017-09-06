7515796471164a44ac5bcc8227fe0df59e4b36c8
Mauro Sebastianelli <majuro@hotmail.it>
2017-09-07 00:45:17 +0200

    sync

---
+++ adminer/autologin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mauro
+ * Date: 07/09/17
+ * Time: 00:34
+ */
+
+class Autologin {
+    function credentials() {
+        if (!empty($_GET['username'])) return array(SERVER, $_GET["username"], get_password());
+        return array($_ENV['ADMINER_SERVER'], $_ENV['ADMINER_USER'], $_ENV['ADMINER_PASSWORD']);
+    }
+}
+
+return new Autologin();
