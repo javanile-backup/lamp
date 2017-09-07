5a0aa62677d3611e6810529ead19a918dc2bdc11
francescobianco <info.francescobianco@gmail.com>
2017-09-07 19:26:13 +0200

    sync

--- adminer/autologin.php
+++ adminer/autologin.php
@@ -8,8 +8,11 @@
 
 class Autologin {
     function credentials() {
-        if (!empty($_GET['username'])) return array(SERVER, $_GET["username"], get_password());
+        //if (!empty($_GET['username'])) return array(SERVER, $_GET["username"], get_password());
         return array($_ENV['ADMINER_SERVER'], $_ENV['ADMINER_USER'], $_ENV['ADMINER_PASSWORD']);
+    }
+    function database() {
+        return $_ENV['ADMINER_DATABASE'];
     }
 }
 
