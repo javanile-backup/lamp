fcc4cc7091638e195dc191f26aa645d8228ce947
Francesco Bianco <info.francescobianco@gmail.com>
2018-01-28 18:19:26 +0100

    sync

--- adminer/autologin.php
+++ adminer/autologin.php
@@ -1,37 +1,31 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: mauro
- * Date: 07/09/17
- * Time: 00:34
+ * Class Autologin
  */
 
-class Autologin {
-    function credentials() {
-        //if (!empty($_GET['username'])) return array(SERVER, $_GET["username"], get_password());
-        //return array($_ENV['ADMINER_SERVER'], $_ENV['ADMINER_USER'], $_ENV['ADMINER_PASSWORD']);
+class Autologin
+{
+    /**
+     * @return array
+     */
+    function credentials()
+    {
         return array($_ENV['MYSQL_HOST'], 'root', $_ENV['MYSQL_ROOT_PASSWORD']);
     }
-    function database() {
+
+    /**
+     * @return mixed
+     */
+    function database()
+    {
         return $_ENV['MYSQL_DATABASE'];
     }
 
-    function loginForm() {
-        /*
-        ?>
-        <table cellspacing="0">
-            <select name='auth[driver]'><option value="server" selected>MySQL</select>
-            <tr><th>Server<td><input name="auth[server]" value="<?=$_ENV['ADMINER_SERVER']?>" title="hostname[:port]" autocapitalize="off">
-            <tr><th>Username<td><input name="auth[username]" id="username" value="<?=$_ENV['ADMINER_USER']?>" autocapitalize="off">
-            <tr><th>Password<td><input type="password" name="auth[password]" value="<?=$_ENV['ADMINER_PASSWORD']?>">
-            <tr><th>Database<td><input name="auth[db]" value="<?=$_ENV['ADMINER_DATABASE']?>" autocapitalize="off" placeholder="WordPress">
-        </table>
-        <p><input id="submit-button" type="submit" value="<?php echo lang('Login'); ?>">
-        <script>document.getElementById("submit-button").click()</script>
-        <?php
-        echo checkbox("auth[permanent]", 1, $_COOKIE["adminer_permanent"], lang('Permanent login')) . "\n";
-        return true;
-        */
+    /**
+     * @return bool
+     */
+    function loginForm()
+    {
         ?>
         <table cellspacing="0">
             <select name='auth[driver]'><option value="server" selected>MySQL</select>
@@ -48,4 +42,5 @@ class Autologin {
     }
 }
 
+// return singleton module
 return new Autologin();
