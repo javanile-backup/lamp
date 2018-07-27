cfb9bbf1bfccdf2bff1454ee0e723bb846e9c14e
Francesco Bianco <info.francescobianco@gmail.com>
2018-07-27 14:50:28 +0200

    fix error logs

--- 7.1/foreground.sh
+++ 7.1/foreground.sh
@@ -2,6 +2,12 @@
 ## LAMP with PHP 7.1
 ## Author: Francesco Bianco <bianco@javanile.org>
 
+## prepare log files
+touch /log/php_error.log \
+      /log/apache2_error.log \
+      /log/apache2_access.log
+chown www-data:www-data -R /log/*.log
+
 ## run cron
 chmod a+x /etc/cron.d/crontab
 crontab /etc/cron.d/crontab
